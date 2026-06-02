c52b9e1ce9ad9939fc5b69f56d2ecafb39d77fed
350375325-web <350375325@gapps.yrdsb.ca>
2026-06-02 12:43:06 -0400

    IntroScreen Created
    Code as been added and commented out -- change as needed

---
+++ Minesweeper/IntroScreen.java
@@ -0,0 +1,31 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class IntroScreen here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class IntroScreen extends World
+{
+    /**
+     * Constructor for objects of class IntroScreen.
+     * 
+     */
+    public IntroScreen() {
+        super(1200, 800, 1); 
+        //setBackground(new GreenfootImage("images/Background/StartWorld.png"));
+        //startButton = new Button("Start", 150);
+        //addObject(startButton, getWidth() / 2, getHeight() / 5 * 4 + 20);
+        //Greenfoot.setWorld(this);
+    }
+    
+    public void act() {
+        //if (Greenfoot.mouseClicked(startButton)) {
+            //stopped();
+            //Greenfoot.setWorld(new StoryWorld()); 
+        //}
+    }
+    
+    
+}
