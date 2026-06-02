148f08a9e394693f75fc59a17bbf791f0b7fe6b4
Cyndi Zou <cyndizou2@gmail.com>
2026-06-02 15:09:52 -0400

    Button Superclass & Menu Button Class
    completed both classes, menu button handles the clicking logic to do what it needs to
    
    includes 4 buttons so far: start, quit, restart, sound
    
    need UI to go with all 4 though

--- Minesweeper/IntroScreen.java
+++ Minesweeper/IntroScreen.java
@@ -14,18 +14,15 @@ public class IntroScreen extends World
      */
     public IntroScreen() {
         super(1200, 800, 1); 
-        //setBackground(new GreenfootImage("images/Background/StartWorld.png"));
-        //startButton = new Button("Start", 150);
-        //addObject(startButton, getWidth() / 2, getHeight() / 5 * 4 + 20);
-        //Greenfoot.setWorld(this);
+        
+        //set background image
+        
+        //add menu buttons to the screen
+        addObject(new MenuButton(MenuButton.START), 600, 500);
+        addObject(new MenuButton(MenuButton.SOUND), 1150, 50);
     }
     
     public void act() {
-        //if (Greenfoot.mouseClicked(startButton)) {
-            //stopped();
-            //Greenfoot.setWorld(new StoryWorld()); 
-        //}
+        //MenuButton class handles the clicks! :)
     }
-    
-    
 }
