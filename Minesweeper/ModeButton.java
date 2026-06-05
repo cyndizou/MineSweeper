3c13c3beeed2f3605c545b23edb8481449a5b4a6
350375325-web <350375325@gapps.yrdsb.ca>
2026-06-05 13:03:35 -0400

    Committing uncomitted files

---
+++ Minesweeper/ModeButton.java
@@ -0,0 +1,33 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Allows the user to choose between Timed and Relaxed, ans then transitions the user
+ * into the game world where it is either timed or stopwatch. 
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class ModeButton extends Button
+{
+    /**
+     * Act - do whatever the StartButton wants to do. This method is called whenever
+     * the 'Act' or 'Run' button gets pressed in the environment.
+     */
+    
+    private boolean timed;
+    public ModeButton(boolean timed) {
+        this.timed = timed;
+        
+        if(timed) {
+            setImage("timed.png");
+        } else {
+            setImage("relaxed.png");
+        }
+    }
+    
+    
+    public void act()
+    {
+        // Add your action code here.
+    }
+}
