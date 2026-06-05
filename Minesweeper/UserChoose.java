35d2865f10a2baa209c44b7d6ac87e418389c043
350375325-web <350375325@gapps.yrdsb.ca>
2026-06-05 12:15:11 -0400

    Uncommitted files?

---
+++ Minesweeper/UserChoose.java
@@ -0,0 +1,24 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * This class is for the user to choose if they want timed or relxed, as well as
+ * which grid size they want. 
+ * 
+ * @author (Aiza) 
+ * @version (a version number or a date)
+ */
+public class UserChoose extends Button
+{
+    /**
+     * Act - do whatever the UserChoose wants to do. This method is called whenever
+     * the 'Act' or 'Run' button gets pressed in the environment.
+     */
+    public void act()
+    {
+        // Add your action code here.
+    }
+    
+    public UserChoose(String label) {
+        
+    }
+}
