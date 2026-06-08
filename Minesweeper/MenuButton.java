fd7101e6061af1c46af02249730df0073479a33e
349070227-hub <349070227@gapps.yrdsb.ca>
2026-06-08 13:09:42 -0400

    changed button images
    old one was too long and didn't look very well with the spacing. a shorter one was generated and the sizes are equal this time! yay!

---
+++ Minesweeper/MenuButton.java
@@ -0,0 +1,111 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class MenuButton here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class MenuButton extends Button
+{
+    //initalize constants for possible menu buttons 
+    public static final String START = "Start";
+    public static final String QUIT = "Quit";
+    public static final String RESTART = "Restart";
+    public static final String SOUND = "Sound";
+    public static final String INFO = "Info";
+
+    //tracks if sound is on or off
+    private boolean soundOn;
+    
+    /**
+     * Constructor - creates a menu button with the given label and sets sound
+     * to on by default
+     */
+    public MenuButton(String label){
+        super(label);
+        soundOn = true;
+        updateImage();
+    }
+    
+    /**
+     * called every frame by greenfoot
+     * checks for clicks and updates the image
+     */
+    public void act()
+    {
+        super.act();
+        if(isClicked()){
+            handleClick();
+        }
+        updateImage();
+    }
+    
+    /**
+     * handles what happens when the button is clicked
+     * depending on what type of button it is
+     */
+    private void handleClick(){
+        if(label.equals(START)){
+            //switch to main game world
+            //commented out going to main world to bringing 
+            //user to customization world
+            //Greenfoot.setWorld(new MyWorld(9));
+            //Greenfoot.setWorld(new CustomizationWorld());
+            
+            if(getWorld() instanceof CustomizationWorld) {
+                CustomizationWorld world = (CustomizationWorld)getWorld();
+                Greenfoot.setWorld(new MyWorld(world.getGridSize(), world.getTimedMode()));
+            } else {
+                Greenfoot.setWorld(new CustomizationWorld());
+            }
+            
+        } else if(label.equals(QUIT)){
+            //stop the game
+            Greenfoot.stop();
+        } else if(label.equals(RESTART)){
+            //restart a new world 
+            //had to add changes here 
+            //Greenfoot.setWorld(new MyWorld(9));
+            
+            MyWorld world = (MyWorld)getWorld();
+            Greenfoot.setWorld(new MyWorld(world.getGridSize(), world.getTimedMode()));
+        } else if (label.equals(SOUND)){
+            //flip sound between on and off
+            soundOn = !soundOn;
+        } else if (label.equals(INFO)) {
+            Greenfoot.setWorld(new InstructionWorld());
+        }
+    }
+    
+    /**
+     * sets the image of this button based on its label
+     */
+    private void updateImage() {
+        if (label.equals(SOUND)) {
+            if (soundOn) {
+                setImage("buttons/sound on.png");
+            } else {
+                setImage("buttons/sound off.png");
+            }
+        } else if (label.equals(START)) {
+            setImage("buttons/start (1).png");
+        } else if (label.equals(QUIT)) {
+            setImage("buttons/quit (1).png");
+        } else if (label.equals(RESTART)) {
+            setImage("buttons/restart (1).png");
+        } else if (label.equals(INFO)) {
+            setImage("buttons/info.png");
+        }
+    }
+    
+    //returns whether sound is currently on, used by other classes 
+    public boolean isSoundOn(){
+        return soundOn;
+    }
+    
+    //resizing the button in the game world
+    public void resize(int width, int height) {
+        getImage().scale(width, height);
+    }
+}
---
+++ Minesweeper/ModeButton.java
@@ -0,0 +1,64 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Allows the user to choose between Timed and Relaxed, ans then transitions the user
+ * into the game world where it is either timed or stopwatch. 
+ * 
+ * @author (Aiza) 
+ * @version (a version number or a date)
+ */
+public class ModeButton extends Button
+{
+    //variables
+    public static final String TIMED = "Timed";
+    public static final String RELAXED = "Relaxed";
+    public static final String EASY = "Easy";
+    public static final String HARD = "Hard"; 
+    
+    public ModeButton(String label) {
+        super(label);
+        updateImage();
+    }
+    
+    
+    public void act()
+    {
+        super.act();
+        if(isClicked()) {
+            handleClick();
+        }
+    }
+    
+    private void handleClick() {
+        CustomizationWorld world = (CustomizationWorld)getWorld();
+        /*
+        if(label.equals(TIMED)) {
+            Greenfoot.setWorld(new MyWorld(9));
+        } else if (label.equals(RELAXED)) {
+            Greenfoot.setWorld(new MyWorld(9));
+        }
+        */
+       
+        if(label.equals(TIMED)) {
+            world.setTimedMode(true);
+        } else if (label.equals(RELAXED)) {
+            world.setTimedMode(false);
+        } else if (label.equals(EASY)) {
+            world.setGridSize(9);
+        } else if (label.equals(HARD)) {
+            world.setGridSize(16);
+        }
+    }
+    
+    private void updateImage() {
+        if(label.equals(TIMED)) {
+            setImage("buttons/timed (1).png");
+        } else if (label.equals(RELAXED)) {
+            setImage("buttons/relaxed (1).png");
+        } else if (label.equals(EASY)) {
+            setImage("buttons/easy (1).png");
+        } else if (label.equals(HARD)) {
+            setImage("buttons/hard (1).png");
+        }
+    }
+}
