61965f94e26378bf2b966b6b8b972a783c8b9d2e
350375325-web <350375325@gapps.yrdsb.ca>
2026-06-12 17:56:45 -0400

    EndWorld and MyWorld fixed for ScoreManager

--- Minesweeper/MyWorld.java
+++ Minesweeper/MyWorld.java
@@ -82,7 +82,7 @@ public class MyWorld extends World
         if(gameLost) {
             endDelay--;
             if(endDelay <= 0) {
-                Greenfoot.setWorld(new EndWorld());
+                Greenfoot.setWorld(new EndWorld(false, 0, gridSize));
             }
         }
     }
@@ -241,7 +241,12 @@ public class MyWorld extends World
                 }
             }
         }
-        Greenfoot.stop();
+        
+        
+        int finalTime = timerDisplay.getTime();
+        ScoreManager.saveScore(finalTime, gridSize);
+        
+        Greenfoot.setWorld(new EndWorld(true, finalTime, gridSize));
     }
     
     /**
