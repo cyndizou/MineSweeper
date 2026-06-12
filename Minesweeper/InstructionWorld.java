38343bf57744bbcf5921c51c3de204c690fa4d58
Cyndi Zou <cyndizou2@gmail.com>
2026-06-12 17:35:30 -0400

    aiza
    Co-Authored-By: 350375325-web <275105403+350375325-web@users.noreply.github.com>

--- Minesweeper/InstructionWorld.java
+++ Minesweeper/InstructionWorld.java
@@ -11,7 +11,6 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
  */
 public class InstructionWorld extends World
 {
-
     /**
      * Constructor for objects of class InstructionClass.
      * 
@@ -20,6 +19,6 @@ public class InstructionWorld extends World
     {    
         // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
         super(800, 600, 1); 
-        setBackground(new GreenfootImage("border.png"));
+        setBackground(new GreenfootImage("instructionsPage2.png"));
     }
 }
